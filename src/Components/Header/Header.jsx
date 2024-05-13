1153144fd433cd195be17a72bb56f43cc98ca3de
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-13 17:30:41 +0100

    style header__left

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton } from "@mui/material";
 
 function Header() {
   return (
     <div className='header'>
-      <h2>This is a header</h2>
+      <div className='header__left'>
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src='https://image.similarpng.com/very-thumbnail/2020/12/Gmail-logo-design-on-transparent-background-PNG.png'
+          alt='gmail-logo'
+        ></img>
+      </div>
+      <div className='header__middle'></div>
+      <div className='header__right'></div>
     </div>
   );
 }
