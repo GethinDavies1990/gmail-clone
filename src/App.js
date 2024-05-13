7bf33b45f6663321e1cddc600849c4810f615c6f
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-13 17:59:21 +0100

    feat: add compose button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <Sidebar />
       <h2>Lets build gmail</h2>
     </div>
   );
