de8122902cba575671b20080662e5f9db3cd0d26
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-18 13:00:33 +0100

    installed react-router dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import Mail from "./Components/MailComponent/Mail";
+import EmailList from "./Components/EmailListComponent/EmailList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <div className='app__body'>
+          <Sidebar />
+          <Routes>
+            <Route path='/mail' element={<Mail />}></Route>
+            <Route path='/' element={<EmailList />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
