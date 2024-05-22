818304fd152e9a184745cb728100e1170577eb06
Gethin Davies <110056268+GethinDavies1990@users.noreply.github.com>
2024-05-22 19:27:34 +0100

    add login function

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import "./Login.css";
+import { Button } from "@mui/material";
+import { auth, provider } from "../../firebase";
+import { login } from "../../features/userSlice";
+import { useDispatch } from "react-redux";
 
 function Login() {
+  const dispatch = useDispatch();
+
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(({ user }) => {
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+          })
+        );
+      })
+      .catch((error) => alert(error.message));
+  };
   return (
     <div className='login'>
       Login
       <div className='login__container'>
         <img src='' alt='' />
+        <Button onClick={signIn}>Login</Button>
       </div>
     </div>
   );
